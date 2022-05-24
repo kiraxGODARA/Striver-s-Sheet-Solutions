7036e098ec921025bfb489815d1b31fd86d87dfc
Ankur godara <90837436+kiraxGODARA@users.noreply.github.com>
2022-05-24 18:44:47 +0530

    Update Day 3 - Q1.cpp

--- Arrays/Day 3 - Q1.cpp
+++ Arrays/Day 3 - Q1.cpp
@@ -6,7 +6,7 @@ public:
         int l = 0, r = m * n - 1;
         while (l != r){
             int mid = (l + r)/2;
-            if (matrix[mid / m][mid % m] < target)
+            if (matrix[mid / m][mid % m] < target).   /* converted matrix to sorted list */
                 l = mid + 1;
             else 
                 r = mid;
